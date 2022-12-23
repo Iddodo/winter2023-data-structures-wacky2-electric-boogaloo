113d2910b5dfe952812f706107cbd6afd809954d
Amitv <amitvais777@gmail.com>
2022-12-23 11:25:00 +0200

    pep8 in both files infinite loop fix in players array fix bug in upper bound of spirit permutation function

--- wacky_tests.py
+++ wacky_tests.py
@@ -6,39 +6,46 @@ import random
 
 # --- Custom variables for test generation --- #
 
-num_players = 1
-num_teams = 1
-num_commands = 1
+num_players = 10
+num_teams = 5
+num_commands = 20
 random_integer_upper_bound = 300
 
 # --- Player and team ID lists --- #
 
 players = [i for i in range(1, num_players + 1)]
+initial_players = players.copy()
 teams = [i for i in range(1, num_teams + 1)]
 removed_players = []
 
 team_players = {ID: [] for ID in teams}
 
+
 # --- Functions to fetch relevant random values --- #
 def random_team_ID():
     return random.choice(teams)
 
-def random_player_ID(include_removed = False):
+
+def random_player_ID(include_removed=False):
     if include_removed:
         return random.choice(players + removed_players)
 
     return random.choice(players)
 
+
 def random_positive_integer():
     return random.randint(1, random_integer_upper_bound)
 
+
 def random_1to5_permutation():
-    identity = [0,1,2,3,4]
+    identity = [0, 1, 2, 3, 4]
     random.shuffle(identity)
     return Permutation(identity)
 
+
 def new_team_ID():
     return max(teams) + 1
+
 
 def new_player_ID():
     return max(players) + 1
@@ -47,7 +54,6 @@ def new_player_ID():
 # --- Randomizing commands ---#
 
 w = Wacky2Unit()
-
 
 test_commands = [
     'add_team',
@@ -64,6 +70,7 @@ test_commands = [
     'buy_team',
 ]
 
+
 def w_new_player(ID):
     r = [random_positive_integer() for i in range(0, 3)]
     team_ID = random_team_ID()
@@ -72,11 +79,11 @@ def w_new_player(ID):
     players.append(ID)
     team_players[team_ID].append(ID)
 
+
 # --- Implementation of runtime commands ---- #
 # Commands with actual function names assure non-invalid input,
 # and prevents trivial failure (some failure may still occur).
 def execute_test_command(cmd):
-
     if cmd == 'add_team':
         w.add_team(new_team_ID())
 
@@ -91,7 +98,6 @@ def execute_test_command(cmd):
         teams.remove(team_ID)
         del team_players[team_ID]
 
-
     if cmd == 'add_player':
         w_new_player(new_player_ID())
 
@@ -101,7 +107,7 @@ def execute_test_command(cmd):
     # Allow player IDs
     if cmd == 'num_played_games_for_player':
         w.num_played_games_for_player(
-            random_player_ID(include_removed = True))
+            random_player_ID(include_removed=True))
 
     # TODO: also test for removed players
     if cmd == 'add_player_cards':
@@ -109,7 +115,7 @@ def execute_test_command(cmd):
 
     if cmd == 'get_player_cards':
         w.get_player_cards(
-            random_player_ID(include_removed = True))
+            random_player_ID(include_removed=True))
 
     if cmd == 'get_team_points':
         w.get_team_points(random_team_ID())
@@ -124,10 +130,11 @@ def execute_test_command(cmd):
     if cmd == 'buy_team':
         w.buy_team(random_team_ID(), random_team_ID())
 
+
 for ID in teams:
     w.add_team(ID)
 
-for ID in players:
+for ID in initial_players:
     w_new_player(ID)
 
 for i in range(0, num_commands):
